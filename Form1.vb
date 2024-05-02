98fdec26ec5aaceaf4475aed243fe4ddebda294e
AlphaThe Hacker <89278252+KingAlpha362@users.noreply.github.com>
2024-05-02 22:15:09 +0200

    Add files via upload
    This is the code that enables the program to run.

---
+++ Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim age As Integer
+        Dim ans As Double
+
+        age = TextBox1.Text
+
+        ans = age * 365 * 24
+
+        MessageBox.Show("You've been alive for " & ans & " hours! ")
+    End Sub
+End Class
